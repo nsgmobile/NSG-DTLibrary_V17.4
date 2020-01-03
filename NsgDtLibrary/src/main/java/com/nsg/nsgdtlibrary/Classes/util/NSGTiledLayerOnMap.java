a2edd65f2125f1c557b9b2a8faf5a56dfeb52bfa
sailaja-cheela <er.sailaja.cheela@gmail.com>
2020-01-03 11:09:39 +0530

    conmmit

--- NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGTiledLayerOnMap.java
+++ NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGTiledLayerOnMap.java
@@ -1638,7 +1638,7 @@ public class NSGTiledLayerOnMap extends Fragment implements View.OnClickListener
 
         destinationMarker= mMap.addMarker(new MarkerOptions()
                 .position(DestinationNode)
-                .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.destination_marker_white_text)));
+                .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.destination_marker_whitetext_lightgreen)));
         CameraPosition googlePlex1 = CameraPosition.builder()
                 .target(DestinationNode)
                 .zoom(18)
