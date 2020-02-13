cd2c175834b9234f665153a7b65e4a5cd97fd9c8
sailaja-cheela <er.sailaja.cheela@gmail.com>
2020-02-13 19:05:10 +0530

    conmmit--changes in ETA, Location,fused api--working navigation test

--- NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGTiledLayerOnMap.java
+++ NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGTiledLayerOnMap.java
@@ -514,37 +514,39 @@ public class NSGTiledLayerOnMap extends Fragment implements View.OnClickListener
                                     locationCallback = new LocationCallback() {
                                         @Override
                                         public void onLocationResult(LocationResult locationResult) {
-                                            if (locationResult == null) {
-                                                return;
-                                            }
-                                            for (Location location : locationResult.getLocations()) {
-                                                if (location != null) {
-                                                    // if(islocationControlEnabled==false) {
-                                                    wayLatitude = location.getLatitude();
-                                                    wayLongitude = location.getLongitude();
-                                                    if (!isContinue) {
-                                                        txtLocation.setText(String.format(Locale.US, "%s - %s", wayLatitude, wayLongitude));
-                                                        //  Log.v("APP DATA", "APP DATA DATA ........ IF " );
-                                                    } else {
-                                                        stringBuilder.append(wayLatitude);
-                                                        stringBuilder.append("-");
-                                                        stringBuilder.append(wayLongitude);
-                                                        stringBuilder.append("\n\n");
-                                                        // txtContinueLocation.setText(stringBuilder.toString());
-                                                        // Log.v("APP DATA", "location DATA ........ELSE  " );
-                                                        currentGPSPosition = new LatLng(wayLatitude, wayLongitude);
-                                                        // Log.v("APP DATA", "currentGPSPosition DATA ........ELSE  "+currentGPSPosition );
+                                             if(islocationControlEnabled==false) {
+                                                 if (locationResult == null) {
+                                                     return;
+                                                 }
+                                                 for (Location location : locationResult.getLocations()) {
+                                                     if (location != null) {
 
-                                                        //  s1= String.valueOf(wayLatitude);
-                                                        //  s2= String.valueOf(wayLongitude);
-                                                        //  Log.v("APP DATA","lat"+s1+"long"+s2);
-                                                    }
-                                                    // }else{
+                                                         wayLatitude = location.getLatitude();
+                                                         wayLongitude = location.getLongitude();
+                                                         if (!isContinue) {
+                                                             txtLocation.setText(String.format(Locale.US, "%s - %s", wayLatitude, wayLongitude));
+                                                             //  Log.v("APP DATA", "APP DATA DATA ........ IF " );
+                                                         } else {
+                                                             stringBuilder.append(wayLatitude);
+                                                             stringBuilder.append("-");
+                                                             stringBuilder.append(wayLongitude);
+                                                             stringBuilder.append("\n\n");
+                                                             // txtContinueLocation.setText(stringBuilder.toString());
+                                                             // Log.v("APP DATA", "location DATA ........ELSE  " );
+                                                             currentGPSPosition = new LatLng(wayLatitude, wayLongitude);
+                                                             // Log.v("APP DATA", "currentGPSPosition DATA ........ELSE  "+currentGPSPosition );
 
-                                                    // }
+                                                             //  s1= String.valueOf(wayLatitude);
+                                                             //  s2= String.valueOf(wayLongitude);
+                                                             //  Log.v("APP DATA","lat"+s1+"long"+s2);
+                                                         }
+                                                         // }else{
 
-                                                }
-                                            }
+                                                         // }
+
+                                                     }
+                                                 }
+                                             }
                                         }
                                     };
                                     //END LOCATION OBJECT
