71420daee4e51cbb83d64a87571205807b5600e9
sailaja-cheela <er.sailaja.cheela@gmail.com>
2020-02-13 19:58:26 +0530

    conmmit--changes in ETA, Location,fused api--working navigation test

--- NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGTiledLayerOnMap.java
+++ NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGTiledLayerOnMap.java
@@ -521,7 +521,11 @@ public class NSGTiledLayerOnMap extends Fragment implements View.OnClickListener
                             @Override
                             public void run() {
                                 if (currentGpsPosition != null && DestinationNode != null) {
-                                    NavigationDirection(currentGpsPosition, DestinationNode);
+                                    if(islocationControlEnabled==false) {
+                                        NavigationDirection(currentGpsPosition, DestinationNode);
+                                    }else{
+
+                                    }
                                 }
                             }
 
@@ -583,37 +587,42 @@ public class NSGTiledLayerOnMap extends Fragment implements View.OnClickListener
                                                 } else {
                                                     Log.e("CurrentGpsPoint", " currentGpsPosition ------ " + currentGpsPosition);
                                                     if (OldNearestPosition != null) {
-                                                        Log.e("CurrentGpsPoint", " curren FRM START NAVI ------ " + currentGpsPosition);
-                                                        Log.e("CurrentGpsPoint", " Old  FRM START NAVI ------ " + OldNearestPosition);
                                                         if(islocationControlEnabled==false) {
+                                                            Log.e("CurrentGpsPoint", " curren FRM START NAVI ------ " + currentGpsPosition);
+                                                            Log.e("CurrentGpsPoint", " Old  FRM START NAVI ------ " + OldNearestPosition);
+                                                            Log.e("CurrentGpsPoint", " islocationControlEnabled FLAG------ " + islocationControlEnabled);
                                                             animateCarMove(mPositionMarker, OldNearestPosition, nPosition, 1000);
-                                                        }else{
+                                                            float bearing = (float) bearingBetweenLocations(OldNearestPosition, nPosition);
+                                                            Log.e("BEARING", "BEARING @@@@@@@ " + bearing);
+                                                            int height = getView().getMeasuredHeight();
+                                                            Projection p = mMap.getProjection();
+                                                            Point bottomRightPoint = p.toScreenLocation(p.getVisibleRegion().nearRight);
+                                                            Point center = new Point(bottomRightPoint.x / 2, bottomRightPoint.y / 2);
+                                                            Point offset = new Point(center.x, (center.y + (height / 4)));
+                                                            LatLng centerLoc = p.fromScreenLocation(center);
+                                                            LatLng offsetNewLoc = p.fromScreenLocation(offset);
+                                                            double offsetDistance = SphericalUtil.computeDistanceBetween(centerLoc, offsetNewLoc);
+                                                            LatLng shadowTgt = SphericalUtil.computeOffset(nPosition, offsetDistance, bearing);
+                                                            caclulateETA(TotalDistanceInMTS, SourceNode, currentGpsPosition, DestinationNode);
+                                                            verifyRouteDeviation(OldGPSPosition, currentGpsPosition, DestinationNode, 40, null);
+
+                                                            AlertDestination(currentGpsPosition);
+                                                            if (bearing > 0.0) {
+                                                                CameraPosition currentPlace = new CameraPosition.Builder()
+                                                                        .target(shadowTgt)
+                                                                        .bearing(bearing).tilt(65.5f).zoom(18)
+                                                                        .build();
+                                                                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(currentPlace), 1000, null);
+                                                            } else {
+
+                                                            }
+
+                                                        }else if(islocationControlEnabled==true){
+                                                            Log.e("CurrentGpsPoint", " islocationControlEnabled FLAG------ " + islocationControlEnabled);
+
                                                             animateCarMoveNotUpdateMarker(mPositionMarker, OldNearestPosition, nPosition, 1000);
                                                         }
-                                                        float bearing = (float) bearingBetweenLocations(OldNearestPosition, nPosition);
-                                                        Log.e("BEARING", "BEARING @@@@@@@ " + bearing);
-                                                        int height = getView().getMeasuredHeight();
-                                                        Projection p = mMap.getProjection();
-                                                        Point bottomRightPoint = p.toScreenLocation(p.getVisibleRegion().nearRight);
-                                                        Point center = new Point(bottomRightPoint.x / 2, bottomRightPoint.y / 2);
-                                                        Point offset = new Point(center.x, (center.y + (height / 4)));
-                                                        LatLng centerLoc = p.fromScreenLocation(center);
-                                                        LatLng offsetNewLoc = p.fromScreenLocation(offset);
-                                                        double offsetDistance = SphericalUtil.computeDistanceBetween(centerLoc, offsetNewLoc);
-                                                        LatLng shadowTgt = SphericalUtil.computeOffset(nPosition, offsetDistance, bearing);
-                                                        caclulateETA(TotalDistanceInMTS, SourceNode, currentGpsPosition, DestinationNode);
-                                                        verifyRouteDeviation(OldGPSPosition, currentGpsPosition, DestinationNode, 40, null);
 
-                                                        AlertDestination(currentGpsPosition);
-                                                        if (bearing > 0.0) {
-                                                            CameraPosition currentPlace = new CameraPosition.Builder()
-                                                                    .target(shadowTgt)
-                                                                    .bearing(bearing).tilt(65.5f).zoom(18)
-                                                                    .build();
-                                                            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(currentPlace), 1000, null);
-                                                        } else {
-
-                                                        }
 
                                                     }
                                                 }
@@ -646,6 +655,7 @@ public class NSGTiledLayerOnMap extends Fragment implements View.OnClickListener
                 if (mMap != null && isNavigationStarted == true && islocationControlEnabled==true ) {
                     isNavigationStarted = false;
                     islocationControlEnabled=false;
+                    Log.e("STOP NAVIGATION", "STOP NAVIGATION");
 
 
                     if (mFusedLocationClient != null) {
@@ -674,6 +684,8 @@ public class NSGTiledLayerOnMap extends Fragment implements View.OnClickListener
                         toast.show();
                     }
                     // getActivity().onBackPressed();
+                    islocationControlEnabled=false;
+                    Log.e("STOP NAVIGATION", " islocationControlEnabled STOP NAVIGATION FLAG "+ islocationControlEnabled);
                 }
             }
 
