340c746f4ee1e811826bb404522e0ac40d80a612
sailaja-cheela <er.sailaja.cheela@gmail.com>
2020-03-03 11:52:55 +0530

    conmmit--changes in ETA, Location,fused api--working navigation test

--- NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGIMapFragmentActivity.java
+++ NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGIMapFragmentActivity.java
@@ -550,7 +550,7 @@ import static java.lang.Math.sin;
 
                             }, 0, 15000);
                         }
-                      //  mMap.setMyLocationEnabled(true);
+                        mMap.setMyLocationEnabled(true);
                         mMap.setBuildingsEnabled(true);
                         mMap.getUiSettings().setZoomControlsEnabled(true);
                         mMap.getUiSettings().setCompassEnabled(true);
