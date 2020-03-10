c0fef2a7c4fdbb40391cdc2cdb4c2da329bdfdb4
sailaja-cheela <er.sailaja.cheela@gmail.com>
2020-03-10 14:02:11 +0530

    conmmit--changes in ETA, Location,fused api--working navigation test

--- app/src/main/java/com/nsg/dtlibrary/NSGApiActivity.java
+++ app/src/main/java/com/nsg/dtlibrary/NSGApiActivity.java
@@ -30,7 +30,7 @@ public class NSGApiActivity extends FragmentActivity implements NSGIMapFragmentA
     private String charlsisNumber;
     private  Button Start,Stop;
     private String jobId="1",routeId;
-/*
+
     private String GeoFenceCordinates="17.455565 78.555251,\n" +
             "17.455483 78.555368,\n" +
             "17.455285 78.555436,\n" +
@@ -38,18 +38,18 @@ public class NSGApiActivity extends FragmentActivity implements NSGIMapFragmentA
             "17.455259 78.554962,\n" +
             "17.455504 78.554935,\n" +
             "17.455702 78.555192";
-*/
 
+/*
     private String GeoFenceCordinates="24.979977 55.067160,\n" +
             "24.979958 55.067257,\n" +
             "24.979864 55.067257,\n" +
             "24.979831 55.067185,\n" +
             "24.979895 55.067124,\n" +
             "24.979979 55.067136";
+*/
 
 
 
-/*
 
     String SourcePosition="78.570759 17.473453";
     String DestinationPosition="78.555174 17.455362";
@@ -290,9 +290,9 @@ public class NSGApiActivity extends FragmentActivity implements NSGIMapFragmentA
             "        }\n" +
             "    ]\n" +
             "}";
-   */
 
 
+/*
     private String SourcePosition = "55.067291 24.978782";
     private String DestinationPosition = "55.067205 24.979878";
     // 25.26886,55.33279   25.27078,55.3327
@@ -324,7 +324,7 @@ public class NSGApiActivity extends FragmentActivity implements NSGIMapFragmentA
             "[55.066794299000037,24.98024651500009],[55.066816470000049,24.980249936000064],[55.066838951000079,24.980250405000049],\n" +
             "[55.066861270000061,24.980247913000085]]}},{\"$id\":\"6\",\"EdgeNo\":\"443\",\"GeometryText\":\"-\",\"Geometry\":\n" +
             "{\"$id\":\"7\",\"type\":\"LineString\",\"coordinates\":[[55.066861270000061,24.980247913000085],[55.0672260238388,24.9799000715094]]}}]}";
-
+*/
 
 /*
     String SourcePosition="55.077054 24.996333";
