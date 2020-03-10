190aa72665a5eec3442f9ed6d87fb46b556b66d5
sailaja-cheela <er.sailaja.cheela@gmail.com>
2020-03-10 12:03:39 +0530

    conmmit--changes in ETA, Location,fused api--working navigation test

--- NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGIMapFragmentActivity.java
+++ NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGIMapFragmentActivity.java
@@ -314,7 +314,7 @@ import static java.lang.Math.sin;
             //Initialise Accuracy
             locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
             //Initialise interval
-            locationRequest.setInterval(10 * 1000); // 10 seconds
+            locationRequest.setInterval(1 * 1000); // 10 seconds
             // locationRequest.setFastestInterval(5 * 1000); // 5 seconds
 
             new GpsUtils(getContext()).turnGPSOn(new GpsUtils.onGpsListener() {
@@ -555,7 +555,7 @@ import static java.lang.Math.sin;
                                     }
                                 }
 
-                            }, 0, 15000);
+                            }, 0, 5000);
                         }
                         mMap.setMyLocationEnabled(true);
                         mMap.setBuildingsEnabled(true);
